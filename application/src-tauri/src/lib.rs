3c55e0ce8276087195e26387ad18b45c99a423ec
Alexey <yankovskyalexey123@yandex.ru>
2025-05-09 18:47:20 +0300

    application added

---
+++ application/src-tauri/src/lib.rs
@@ -0,0 +1,45 @@
+use {std::io::Write,
+     scheduler::{Schedule, models::{csv, ScheduleModel}}};
+
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn work_with(file: &str) -> String {
+    let file = file.to_string();
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(file.as_bytes());
+    // let mut schedule: Schedule = serde_json::from_str::<ScheduleModel>(&file).unwrap().into();
+    let mut schedule = Schedule::new(ScheduleModel::deserialize_csv(&mut reader).unwrap().into());
+
+    let aging = 10000;//args.aging_opt.unwrap_or(scheduler::AGING_OPT_DEFAULT);
+
+    let time = std::time::Instant::now();
+
+    schedule.optimize(
+        0.999,
+        aging,
+        true,
+        true,
+        || (),
+    );
+    let dur = time.elapsed();
+    println!("results cost: {}", schedule.cost);
+    println!("calculation time: {}", dur.as_secs_f32());
+    
+    let mut writer = csv::WriterBuilder::new()
+        .has_headers(false)
+        .from_writer(vec![]);
+    ScheduleModel::from(schedule)
+        .serialize_csv(&mut writer)
+        .unwrap();
+    String::from_utf8(writer.into_inner().unwrap()).unwrap()
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![work_with])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ application/src/main.js
@@ -0,0 +1,18 @@
+const { invoke } = window.__TAURI__.core;
+
+let fileInputEl;
+let resultEl;
+
+async function send_file() {
+  console.log(await fileInputEl.files[0].text());
+  result.textContent = await invoke("work_with", { file: await fileInputEl.files[0].text() });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  fileInputEl = document.querySelector("#file-input");
+  resultEl = document.querySelector("#result");
+  document.querySelector("#file-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    send_file();
+  });
+});
