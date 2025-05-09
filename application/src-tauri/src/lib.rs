d66115fd3ee46b922daf7f0514130fc80f0be66a
Alexey <yankovskyalexey123@yandex.ru>
2025-05-09 20:34:47 +0300

    added file output

--- application/src-tauri/src/lib.rs
+++ application/src-tauri/src/lib.rs
@@ -1,9 +1,14 @@
-use {std::io::Write,
-     scheduler::{Schedule, models::{csv, ScheduleModel}}};
+use {
+    scheduler::{
+        models::{csv, ScheduleModel},
+        Schedule,
+    },
+    std::{fs::File, io::Write},
+};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
-fn work_with(file: &str) -> String {
+fn work_with(file: &str, path: &str) -> String {
     let file = file.to_string();
     let mut reader = csv::ReaderBuilder::new()
         .has_headers(false)
@@ -11,33 +16,30 @@ fn work_with(file: &str) -> String {
     // let mut schedule: Schedule = serde_json::from_str::<ScheduleModel>(&file).unwrap().into();
     let mut schedule = Schedule::new(ScheduleModel::deserialize_csv(&mut reader).unwrap().into());
 
-    let aging = 10000;//args.aging_opt.unwrap_or(scheduler::AGING_OPT_DEFAULT);
+    let aging = 10000; //args.aging_opt.unwrap_or(scheduler::AGING_OPT_DEFAULT);
 
     let time = std::time::Instant::now();
 
-    schedule.optimize(
-        0.999,
-        aging,
-        true,
-        true,
-        || (),
-    );
+    schedule.optimize(0.999, aging, true, true, || ());
     let dur = time.elapsed();
     println!("results cost: {}", schedule.cost);
     println!("calculation time: {}", dur.as_secs_f32());
-    
+    println!("{}", path);
+
     let mut writer = csv::WriterBuilder::new()
         .has_headers(false)
-        .from_writer(vec![]);
+        .from_path(&path.trim())
+        .unwrap();
     ScheduleModel::from(schedule)
         .serialize_csv(&mut writer)
         .unwrap();
-    String::from_utf8(writer.into_inner().unwrap()).unwrap()
+    "Success".to_string()
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![work_with])
         .run(tauri::generate_context!())
